61867c03b9f237c01e06b538d0f798bb6a932154
David Suárez <davidfabio.suarezalvarez@usp.ceu.es>
2024-09-25 00:35:18 +0200

    Segundo commit

---
+++ Actividad3/src/Comprobacion.java
@@ -0,0 +1,10 @@
+public class Comprobacion {
+    
+    public static void realizarComprobacion() {
+        Habitante habitante = new Habitante("Víctor","Rodriguez","De la Serna");
+        System.out.println(habitante.getNombre());
+        System.out.println(habitante.getApellido1());
+        System.out.println(habitante.getApellido2());
+       }
+}
+
---
+++ Actividad3/src/Padron.java
@@ -0,0 +1,20 @@
+public class Padron {
+        private String nombre;
+        private String apellido1;
+        private String apellido2;
+
+        public void setNombre(String nombreHabitante){
+            nombre = nombreHabitante;
+        }
+        
+        public void setApellido1(String apellido1Habitante){
+            apellido1 = apellido1Habitante;
+        }
+
+        public void setApellido2(String apellido2Habitante){
+            apellido2 = apellido2Habitante;
+        }
+
+
+    
+    }
