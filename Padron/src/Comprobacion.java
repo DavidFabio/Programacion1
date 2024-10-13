2df9c139ef343c330e36c7abcd039624fbc6cddd
David Fabio <davidfabio.suarezalvarez@usp.ceu.es>
2024-10-13 18:34:34 +0200

    Tercer commit

--- Padron/src/Comprobacion.java
+++ Padron/src/Comprobacion.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
-public class Comprobacion {
-    
-    public static void realizarComprobacion() {
-        Habitante habitante1 = new Habitante("Víctor","Rodriguez","De la Serna");
-        Habitante habitante2 = new Habitante("Jorge", "Salvaje", "Jordi");
-        OficinaPadron padron = new OficinaPadron();
-        padron.annadir(habitante1);
-        padron.annadir(habitante2);
-        ArrayList<Habitante> habitantes = padron.getHabitantesPadron();
-        for(Habitante habitante: habitantes){
-            System.out.println(habitante.getNombre() + habitante.getApellido1() + habitante.getApellido2());
-        }
-        System.out.println(padron.sumarHabitantes());
-       }
+public class Comprobacion
+{
+public static void realizarComprobacion(){
+    OficinaPadron padron = new OficinaPadron();
+    ArrayList<Habitante> habitantes = padron.getHabitantesPadron();
+    for(Habitante habitante : habitantes){
+        System.out.println(habitante.getNombre() + " " +
+        habitante.getApellido1() + " " +
+        habitante.getApellido2());
+    }
+    System.out.println("El número total de habitantes es " + padron.calcularNumeroHabitantes());
+    }
 }
 
--- Padron/src/OficinaPadron.java
+++ Padron/src/OficinaPadron.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class OficinaPadron {
@@ -5,6 +8,7 @@ public class OficinaPadron {
     
     public void annadir(Habitante habitante){
         habitantesPadron.add(habitante);
+        volcarContactos();
     }
     public ArrayList getHabitantesPadron(){
     return habitantesPadron;
@@ -16,5 +20,51 @@ public class OficinaPadron {
             suma++;
         }
         return suma;
+    }
+
+    private void cargarHabitantes(){
+    try{
+        File fichero = new File("padron.csv");
+        // Crea el fichero si no existe:
+        fichero.createNewFile();
+        Scanner sc = new Scanner(fichero);
+        sc.useDelimiter(",|\n");
+        while(sc.hasNext()){
+            Habitante habitante = new Habitante(sc.next(),
+            sc.next(),
+            sc.next());
+            habitantesPadron.add(habitante);
+        }
+        sc.close();
+    }
+    catch(IOException ex){
+        System.out.println("No hay habitantes inscritos");
+    }
+    }
+
+    public OficinaPadron(){
+        cargarHabitantes();
+    }
+
+    public int calcularNumeroHabitantes(){
+        int suma = 0;
+        for(Habitante habitante : habitantesPadron){
+        suma++;
+        }
+        return suma;
+    }
+
+    public void volcarContactos(){
+        try{
+            FileWriter fw = new FileWriter("padron.csv");
+            for(Habitante habitante : habitantesPadron){
+                fw.write(habitante.getNombre() + "," +
+                    habitante.getApellido1() + "," +
+                    habitante.getApellido2() + "n");
+            }
+            fw.close();
+        }catch(IOException ex){
+            System.err.println(ex);
+        }
     }
 }
